e5ffd902bef2ccf70d249ccc11d68596b25c82d9
Lachlan Gordon <lachlanwgordon@gmail.com>
2021-05-14 08:32:57 +1000

    Created basic self hosting Ooui Forms

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Ooui;
+using Ooui.Forms;
+using Xamarin.Forms;
+using Label = Xamarin.Forms.Label;
+namespace PiOoui
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Ooui");
+            UI.Host = "10.1.1.155";
+            Forms.Init();
+            var page = new ContentPage
+            {
+                Content = new StackLayout
+                {
+                    Children = 
+                    {
+                        new Label{ Text = "Hello"}
+                    }
+                }
+            };
+            var element = page.GetOouiElement();
+            UI.Publish("/", element);
+
+            Console.ReadLine();
+
+        }
+    }
+}

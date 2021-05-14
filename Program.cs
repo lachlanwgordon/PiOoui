233a0da3aea77273caa3d494c76ac01bbe071811
Lachlan Gordon <lachlanwgordon@gmail.com>
2021-05-14 14:00:20 +1000

    published a second page and added url for mac
    Updated pr

--- Program.cs
+++ Program.cs
@@ -12,10 +12,14 @@ namespace PiOoui
         {
             Console.WriteLine("Starting Ooui");
             //UI.Host = "10.1.1.155";//This line needed if running on pi and serving to other computers
+            UI.Host = "10.1.1.199";//This line needed if running on mac and serving to other computers
             Forms.Init();
             var page = new XamlPage();
+            var page2 = new HomePage();
             var element = page.GetOouiElement();
+            var element2 = page2.GetOouiElement();
             UI.Publish("/", element);
+            UI.Publish("/test", element2);
 
             Console.ReadLine();
 
